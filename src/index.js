08acd3cc5eb5582c61f347acaf3e071935d32813
tanya-yu <tanya_yu_95@mail.ru>
2020-01-10 01:21:09 +0500

    note cards ui

--- src/index.js
+++ src/index.js
@@ -1,39 +1,45 @@
 import {MDCTopAppBar} from '@material/top-app-bar';
+import {MDCRipple} from '@material/ripple';
 
-// Instantiation
+const selector = '.mdc-button, .mdc-icon-button, .mdc-card__primary-action';
+const ripples = [].map.call(document.querySelectorAll(selector), function(el) {
+  return new MDCRipple(el);
+});
+
 const topAppBarElement = document.querySelector('.mdc-top-app-bar');
 const topAppBar = new MDCTopAppBar(topAppBarElement);
 
 const notesList = document.querySelector('#notes-list');
+var noteTemplate = document.querySelector('#note-template');
+var keyTemplate = document.querySelector('#keyword-template');
 
-function renderNotes(doc) {
+var db = firebase.firestore();
+db.collection('notes').get().then(snapshot => {
+    snapshot.docs.forEach(doc => {
+        renderNote(doc);
+    });
+});
+
+function renderNote(doc) {
     console.log(`${doc.id} => ${doc.data().description}`);
-        
-    let root = document.createElement('div');
-    let description = document.createElement('p');
-    let keywords = document.createElement('div');
+    var note = noteTemplate.content.cloneNode(true);
+    var card = note.querySelector(".note-card");
+    var description = note.querySelector(".note-text");
+    var card = note.querySelector(".note-card");
+    var keywords = note.querySelector(".note-keywords-chips");
 
-    root.setAttribute('data-id', doc.id);
+    card.setAttribute('data-id', doc.id);
     description.textContent = doc.data().description;
 
     var keywordsData = doc.data().keywords;
     var i = 0;
     for(i = 0; i < keywordsData.length; i++) {
-        let keyword = document.createElement('span');
-        keyword.textContent = keywordsData[i];
+        var keyword = keyTemplate.content.cloneNode(true);
+        var keywordText = keyword.querySelector(".mdc-chip__text");
+        keywordText.textContent = keywordsData[i];
         keywords.appendChild(keyword);
     }
 
-    root.appendChild(description);
-    root.appendChild(keywords);
-
-    notesList.appendChild(root);
+    notesList.appendChild(note);
 }
-
-var db = firebase.firestore();
-db.collection('notes').get().then(snapshot => {
-    snapshot.docs.forEach(doc => {
-        renderNotes(doc);
-    });
-});
 
